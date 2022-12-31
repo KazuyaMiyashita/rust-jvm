777ecb88b7228b2148e281f56f73e78580bbd3b7
KazuyaMiayshita <miy1924@gmail.com>
2022-12-31 23:07:36 +0900

    WIP

--- src/class_file.rs
+++ src/class_file.rs
@@ -126,17 +126,42 @@ impl ClassFile {
         // - This is done with ClassFile::read_from_bytes
 
         // The constant pool must satisfy the constraints documented throughout §4.4.
-        // - タグの範囲
+        // - タグの範囲は検証済みである。
         //
         // -- ConstantClassInfo の場合、 name_index は以下を満たす
         // The value of the name_index item must be a valid index into the constant_pool table.
         // The constant_pool entry at that index must be a CONSTANT_Utf8_info structure (§4.4.7) representing a valid binary class
         // or interface name encoded in internal form (§4.2.1).
-        // -- TODO..
+        let constant_pool_constant_class_info_name_indexes= self.constant_pool.cp_infos.iter()
+            .flat_map(|x| match x {  CpInfo::ConstantClassInfo { name_index, .. } => Some(*name_index), _ => None });
+        for constant_pool_constant_class_info_name_index in constant_pool_constant_class_info_name_indexes {
+            match self.constant_pool.get_constant_pool_info(constant_pool_constant_class_info_name_index as usize) {
+                CpInfo::ConstantUtf8Info { tag: CONSTANT_UTF8, .. } => (),
+                _ => panic!("The constant_pool entry at ConstantClassInfo's name_index must be a CONSTANT_Utf8_info structure!")
+            }
+        }
+
+        // -- ConstantFieldrefInfo, ConstantMethodrefInfo, ConstantInterfaceMethodrefInfo に関して
+        // class_index は constant_pool table の有効なインデックスである必要がある。
+
+
+        // QUESTION:
+        // さらに、 ConstantFieldrefInfo の場合、その class_index が指し示すものはクラス型またはインターフェイス型、
+        // ConstantMethodrefInfo の場合、その class_index が指し示すものはクラス型でありインターフェース型ではない、
+        // ConstantInterfaceMethodrefInfo の場合、 class_index が指し示すものはインターフェース型でありクラス型ではないとあるが、
+        // これは検証する必要があるのだろうか？(検証が可能なのだろうか？)
+        // ConstantClassInfo はクラスまたはインターフェースを表すために利用されるものだが、
+        // クラスかインターフェースかを区別する識別子は ConstantClassInfo の中にはない。
+        //
+        // In a CONSTANT_Fieldref_info structure, the class_index item may be either a class type or an interface type.
+        // In a CONSTANT_Methodref_info structure, the class_index item must be a class type, not an interface type.
+        // In a CONSTANT_InterfaceMethodref_info structure, the class_index item must be an interface type, not a class type.
+        // cf. https://docs.oracle.com/javase/specs/jvms/se17/html/jvms-4.html#jvms-4.4.2
 
         // All field references and method references in the constant pool must have valid names, valid classes, and valid descriptors (§4.3).
         // -- TODO..
 
+        todo!()
     }
 
     pub fn load(bytes: &[u8]) -> ClassFile {
@@ -803,7 +828,7 @@ fn test() {
 
     let class_file = ClassFile::load(bytes);
 
-    // println!("{:#04x?}", class_file);
+    println!("{:#04x?}", class_file);
 
     assert_eq!(class_file, ClassFile {
         magic: [0xca, 0xfe, 0xba, 0xbe],
