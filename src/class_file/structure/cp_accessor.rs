ad0b825945f22a92e3abc9925dfb7f0294161530
KazuyaMiayshita <miy1924@gmail.com>
2023-01-07 08:08:50 +0900

    refactor cp_accessor 2

--- src/class_file/structure/cp_accessor.rs
+++ src/class_file/structure/cp_accessor.rs
@@ -1,54 +1,103 @@
 use super::*;
+use thiserror::Error;
 
+pub type Result<T> = std::result::Result<T, AccessError>;
+
+#[derive(Error, Debug, Clone, PartialEq)]
+#[error("Invalid class file. {message:}")]
+pub struct AccessError {
+    pub message: String,
+}
+
+// utils
+fn error<T>(message: String) -> Result<T> {
+    Err(AccessError { message })
+}
+
+fn cp_info_name(cp_info: &CpInfo) -> &str {
+    match cp_info {
+        CpInfo::Utf8(_) => "CONSTANT_Utf8",
+        CpInfo::Integer(_) => "CONSTANT_Integer",
+        CpInfo::Float(_) => "CONSTANT_Float",
+        CpInfo::Long(_) => "CONSTANT_Long",
+        CpInfo::Double(_) => "CONSTANT_Double",
+        CpInfo::Class(_) => "CONSTANT_Class",
+        CpInfo::String(_) => "CONSTANT_String",
+        CpInfo::Fieldref(_) => "CONSTANT_Fieldref",
+        CpInfo::Methodref(_) => "CONSTANT_Methodref",
+        CpInfo::InterfaceMethodref(_) => "CONSTANT_InterfaceMethodref",
+        CpInfo::NameAndType(_) => "CONSTANT_NameAndType",
+        CpInfo::MethodHandle(_) => "CONSTANT_MethodHandle",
+        CpInfo::MethodType(_) => "CONSTANT_MethodType",
+        CpInfo::Dynamic(_) => "CONSTANT_Dynamic",
+        CpInfo::InvokeDynamic(_) => "CONSTANT_InvokeDynamic",
+        CpInfo::Module(_) => "CONSTANT_Module",
+        CpInfo::Package(_) => "CONSTANT_Package",
+    }
+}
 
 // original constant_pool table is indexed from 1 to constant_pool_count - 1.
 // Note that the Vec of this cp_infos structure is indexed from 0.
-fn get_constant_pool_info(constant_pool: &Vec<CpInfo>, index: usize) -> Option<&CpInfo> {
-    constant_pool.get(index - 1)
+fn get_constant_pool_info(constant_pool: &Vec<CpInfo>, index: u16) -> Result<&CpInfo> {
+    match constant_pool.get((index as usize) - 1) {
+        Some(cp_info) => Ok(cp_info),
+        None => error(format!("the index of constant_pool not found! index: {}", index)),
+    }
 }
 
-trait CpAccessor {
-    fn access_as_class<'a>(&'a self, cp: &'a CpInfo) -> ClassCpAccessor<'a>;
+pub trait CpAccessor {
+    // indexed from 1 to constant_pool_count - 1.
+    fn access_as_class<'a>(&'a self, index: u16) -> ClassCpAccessor<'a>;
 }
 
 impl CpAccessor for &Vec<CpInfo> {
-    fn access_as_class<'a>(&'a self, cp: &'a CpInfo) -> ClassCpAccessor<'a> {
-        match cp {
-            CpInfo::Class(info) => ClassCpAccessor { constant_pool: *self, class_info: Some(info) },
-            _ => ClassCpAccessor { constant_pool: *self, class_info: None },
+    fn access_as_class<'a>(&'a self, index: u16) -> ClassCpAccessor<'a> {
+        match get_constant_pool_info(self, index) {
+            Ok(cp) => {
+                match cp {
+                    CpInfo::Class(info) => ClassCpAccessor { constant_pool: *self, class_info_or_err: Ok(info) },
+                    other_info => ClassCpAccessor { constant_pool: *self, class_info_or_err: error(format!("Must specify index of CONSTANT_Class_info but {} found! index: {}", cp_info_name(other_info), index)) },
+                }
+            }
+            Err(e) => ClassCpAccessor { constant_pool: *self, class_info_or_err: Err(e) }
         }
     }
 }
 
-struct ClassCpAccessor<'a> {
+pub struct ClassCpAccessor<'a> {
     constant_pool: &'a Vec<CpInfo>,
-    class_info: Option<&'a ConstantClassInfo>,
+    class_info_or_err: Result<&'a ConstantClassInfo>,
 }
 
 impl ClassCpAccessor<'_> {
     fn name(&self) -> Utf8CpAccessor {
-        match self.class_info {
-            Some(class_info) => {
-                match get_constant_pool_info(&self.constant_pool, class_info.name_index as usize) {
-                    Some(CpInfo::Utf8(info)) => Utf8CpAccessor { constant_pool: self.constant_pool, utf8_info: Some(info) },
-                    _ => Utf8CpAccessor { constant_pool: self.constant_pool, utf8_info: None }
+        match &self.class_info_or_err {
+            Ok(class_info) => {
+                match get_constant_pool_info(&self.constant_pool, class_info.name_index) {
+                    Ok(CpInfo::Utf8(info)) => Utf8CpAccessor { constant_pool: self.constant_pool, utf8_info_or_err: Ok(&info) },
+                    Ok(other_info) => Utf8CpAccessor {
+                        constant_pool: self.constant_pool,
+                        utf8_info_or_err: error(format!("The name_index must refer to CONSTANT_Utf8_info structure, but {} found! name_index: {}", cp_info_name(other_info), class_info.name_index)),
+                    },
+                    Err(e) => Utf8CpAccessor { constant_pool: self.constant_pool, utf8_info_or_err: Err(e) }
                 }
             }
-            None => Utf8CpAccessor { constant_pool: self.constant_pool, utf8_info: None }
+            Err(e) => Utf8CpAccessor { constant_pool: self.constant_pool, utf8_info_or_err: Err(e.to_owned()) }
         }
     }
 }
 
-struct Utf8CpAccessor<'a> {
+pub struct Utf8CpAccessor<'a> {
     constant_pool: &'a Vec<CpInfo>,
-    utf8_info: Option<&'a ConstantUtf8Info>,
+    utf8_info_or_err: Result<&'a ConstantUtf8Info>,
 }
 
 impl Utf8CpAccessor<'_> {
-    fn bytes_as_string(&self) -> Option<String> {
-        match self.utf8_info {
-            Some(utf8_info) => String::from_utf8(utf8_info.bytes.clone()).ok(),
-            None => None
+    fn bytes_as_string(&self) -> Result<String> {
+
+        match &self.utf8_info_or_err {
+            Ok(utf8_info) => String::from_utf8(utf8_info.bytes.clone()).or_else(|e| error(e.to_string())),
+            Err(e) => Err(e.to_owned())
         }
     }
 }
@@ -76,7 +125,7 @@ fn test() {
         CpInfo::Utf8(ConstantUtf8Info { tag: 0x01, length: 0x0c, bytes: "Sample1.java".as_bytes().to_vec() }),
     ];
 
-    let str = constant_pool.access_as_class(&constant_pool[7]).name().bytes_as_string();
+    let str = constant_pool.access_as_class(8).name().bytes_as_string();
 
-    assert_eq!(str, Some("Sample1".to_string()))
+    assert_eq!(str, Ok("Sample1".to_string()))
 }
